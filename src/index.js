616feeabd3bafa106b802b74ad59184e26fb1ecc
Abbas Gawali <abbasgawali111@gmail.com>
2024-07-18 11:20:38 +0530

    completed fetch weather functionality and forecast

--- src/index.js
+++ src/index.js
@@ -8,33 +8,58 @@ const mainFeels = document.getElementById("mainFeels");
 const mainDate = document.getElementById("date");
 const cards = document.querySelector(".cards");
 
+const loader = document.querySelector(".loader");
+const mainContainer = document.querySelector(".maincontainer");
+let toggleLoader = false;
+
+if (toggleLoader) {
+    loader.style.display = "block";
+} else {
+    loader.style.display = "none";
+}
+
 const date = new Date();
 mainDate.textContent = date.toLocaleDateString();
 
 
 const inputCity = document.querySelector("input");
 const searchBtn = document.querySelector("#search")
+const currentLocationBtn = document.querySelector("#currentLocationBtn")
+
+let userLatitude = undefined;
+let userLongitude = undefined;
 
 
 searchBtn.addEventListener("click", () => {
-console.log("btn clicked")
+    if (!inputCity.value) {
+        alert("Please enter City Name");
+    } else {
+
+        fetchForecast(false, false, inputCity.value);
+        showPosition(false, inputCity.value);
+        console.log("btn clicked")
+        console.log(inputCity.value)
+    }
+})
+
+
+currentLocationBtn.addEventListener("click", () => {
+    getCurrentUserLocation();
 })
 
 
 
 
-const getCurrentUserLocation = () => {
-    let userLatitude = undefined;
-    let userLongitude = undefined;
-    function getLocation() {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(showPosition, showError);
-        } else {
-            console.log("Geolocation is not supported by this browser, try in another one.");
-        }
-    }
 
-    const showPosition = async (position) => {
+const showPosition = async (position, location = false) => {
+    if (location) {
+        console.log("location bel")
+        console.log(location)
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${"58eaec513d918c56e6a52478dda1c8e8"}&units=metric`)
+        const data = await response.json();
+        setMainData(data);
+    } else {
+
         userLatitude = position.coords.latitude;
         userLongitude = position.coords.longitude;
         console.log(`Latitude: ${userLatitude}, Longitude: ${userLongitude}`);
@@ -45,6 +70,18 @@ const getCurrentUserLocation = () => {
         fetchForecast(userLatitude, userLongitude);
         console.log(data);
     }
+}
+const getCurrentUserLocation = () => {
+
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition, showError);
+        } else {
+            console.log("Geolocation is not supported by this browser, try in another one.");
+        }
+    }
+
+
     const showError = (err) => {
         alert(err.message)
         return console.warn(err)
@@ -53,11 +90,37 @@ const getCurrentUserLocation = () => {
 }
 
 
-const fetchForecast = async (userLatitude, userLongitude) => {
+const fetchForecast = async (userLatitude, userLongitude, location = false) => {
+    let response = undefined;
+    if (location) {
+        console.log("called for only location")
+        console.log(location)
+        try {
 
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${userLatitude}&lon=${userLongitude}&appid=${"58eaec513d918c56e6a52478dda1c8e8"}&units=metric`);
+            response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${"58eaec513d918c56e6a52478dda1c8e8"}&units=metric`);
+        } catch (error) {
+            console.log("err occured !!!!!!!!!!!!!!!!")
+            console.log(error)
+        }
+
+    } else {
+        try {
+
+            response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${userLatitude}&lon=${userLongitude}&appid=${"58eaec513d918c56e6a52478dda1c8e8"}&units=metric`);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     const data = await response.json();
     console.log(data);
+    console.log(data.cod);
+
+    if (data.cod == 404) {
+        alert("Invalid City")
+        document.location.reload();
+        console.log(data.cod)
+    }
 
     const forecast = data.list.filter((item, index) => {
         return index % 8 === 0;
